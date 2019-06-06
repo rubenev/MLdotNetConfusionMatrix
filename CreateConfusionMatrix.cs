19903a16e8c666288b57be599090dca080566ed9
rubenev <mac_fley@hotmail.com>
2019-06-06 15:10:50 +0200

    Update CreateConfusionMatrix.cs

--- CreateConfusionMatrix.cs
+++ CreateConfusionMatrix.cs
@@ -26,7 +26,9 @@ namespace Some.Actions
         {          
             var sizeTestSet = testData.GetColumn<string>(mlContext, featuresColumn).Count(); 
             var testSetFeatures = testData.GetColumn<string>(mlContext, featuresColumn).Take(sizeTestSet).ToArray(); 
+            // The features are raw meaning the are still strings and not array's of number
             var testSetLabels = testData.GetColumn<bool>(mlContext, labelsColumn).Take(sizeTestSet).ToArray(); 
+            // The labels are pulled parallel from the features
             this.loadedModel = mlContext.Model.Load(this.fileSystemRepository.GetModelFileStream(modelId));
 
             return (sizeTestSet, testSetFeatures, testSetLabels);
@@ -39,13 +41,13 @@ namespace Some.Actions
             var predictionFunction = this.loadedModel.CreatePredictionEngine<ClassifierDataSet, ClassifierPrediction>(mlContext);
             for (var i = 0; i < sizeTestSet; i++)
             {
-                var prediction = predictionFunction.Predict(new ClassifierDataSet()
+                var prediction = predictionFunction.Predict(new ClassifierDataSet() //making a prediction with a model already saved within the application
                 {
                     SentimentText = testSetFeatures[i]
-                });
-                if (prediction.Prediction)
+                }); 
+                if (prediction.Prediction) // if prediction returns with 1/true (positive)
                 {
-                    if (testSetLabels[i])
+                    if (testSetLabels[i]) // if label is 1/true aswell
                     {
                         confusionMatrixModel.TruePositives++;
                     }
@@ -54,9 +56,9 @@ namespace Some.Actions
                         confusionMatrixModel.FalsePositives++;
                     }
                 }
-                else
+                else // if prediction returns with 0/false (negative)
                 {
-                    if (testSetLabels[i] == false)
+                    if (testSetLabels[i] == false)  // if label is 0/false aswell
                     {
                         confusionMatrixModel.TrueNegatives++;
                     }
